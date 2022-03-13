2f0f88b0cbad08f20c83b508c0a6994f8922f3df
arpita <lit2021018@iiitl.ac.in>
2022-03-13 23:34:23 +0530

    issue5

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -14,8 +14,8 @@ const botImg = document.querySelector(".bot-pic");
 let round = 1;
 let max = 1;
 
-playerImg.src = "user.png";
-botImg.src = "bot.png";
+playerImg.src = "resources\images\user.png";
+botImg.src = "resources\images\bot.png";
 function getCompChoice() {
   const choices = ["r", "p", "s", "l", "o"];
   return choices[Math.floor(Math.random() * 5)];
